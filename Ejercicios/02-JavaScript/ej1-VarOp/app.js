74289bec6574a2a6c73e3c723a4f953d22109105
Joaquin <joaquinvannucci@gmail.com>
2026-04-13 10:11:13 -0300

    ejercicio 1 - Variables y operaciones

---
+++ Ejercicios/02-JavaScript/ej1-VarOp/app.js
@@ -0,0 +1,13 @@
+const nombre = 'Joaquin';
+const apellido = 'Vannucci';
+const edad = 24;
+const materia = 'Desarrollo Software'
+
+console.log(`me llamo ${nombre} ${apellido}, tengo ${edad} y estoy cursando ${materia},`); 
+
+let cont = 0;
+
+for (let i = 0; i <3; i++){
+    cont++;
+    console.log(`contador: ${cont}.`);
+}
