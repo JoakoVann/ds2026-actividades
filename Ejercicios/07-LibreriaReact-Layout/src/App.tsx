fb4a6d1a8d412197ad667901d67b2e5d79a22d7f
Joaquin Vannucci <joaquinvannucci@gmail.com>
2026-06-06 15:46:56 -0300

    refactor: estructura de carpetas

---
+++ Ejercicios/07-LibreriaReact-Layout/src/App.tsx
@@ -0,0 +1,20 @@
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+// import Libros from './pages/LibrosCatalogo';
+import { Routes, Route } from 'react-router-dom';
+// import { ContactPage } from './pages/ContactPage'; <-- agregar
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/"            element={<Home />} />
+        {/* <Route path="/catalogo"    element={<Libros />} /> */}
+        {/* <Route path="/contacto"    element={<ContactPage />} /> */}
+      </Routes>
+    </Layout>
+
+  );
+}
+
+export default App;
